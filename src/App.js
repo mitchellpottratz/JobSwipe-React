c3b1c2d41e698041e8513e1a69f4b0feb1af8b19
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-03-16 18:41:07 -0500

    installed react bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 // component imports 
